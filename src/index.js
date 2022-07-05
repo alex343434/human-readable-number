47e4676b45d38922f0eef87d6204f73a8331d888
alex343434 <iquo128@yahoo.com>
2022-07-06 00:08:00 +0500

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function toReadable (number) {
-  
-}
+  const numerals = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
+  ];
+  const dozens = [
+    '', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
+  ];
+  function hundredsToString (num) {
+    return num > 0 ? `${numerals[num]} hundred` : '';
+  }
+  function dozensToString (num) {
+    if (num < 20) {
+        return numerals[num];
+    }
+    if (num % 10 === 0) {
+        return dozens[num / 10];
+    }
+    return `${dozens[Math.floor(num / 10)]} ${numerals[num % 10]}`
+  }
+  const parsedNum = number.toString().split('');
+  var result = [];
+
+  switch (parsedNum.length) {
+    case 3:
+        result.push(hundredsToString(+parsedNum[0]));
+    case 2:
+        var lastNum = parsedNum.at(-2) + parsedNum.at(-1);
+        result.push(dozensToString(+lastNum));
+        break;
+    case 1:
+        result.push(number ? dozensToString(number) : 'zero');
+  }
+  return result.join(' ').trim();
+};
